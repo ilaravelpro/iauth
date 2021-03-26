43e47033c078fb8a9bacf66447c3afe7ff455b5e
bitcoder <me@iamir.net>
2021-03-27 01:09:16 +0430

    up

--- src/iapp/Http/Controllers/API/v1/Auth/Me.php
+++ src/iapp/Http/Controllers/API/v1/Auth/Me.php
@@ -10,17 +10,7 @@
 
 namespace iLaravel\iAuth\iApp\Http\Controllers\API\v1\Auth;
 
-use iLaravel\Core\iApp\Http\Requests\iLaravel as Request;
-
 trait Me
 {
-    public function me(Request $request)
-    {
-        $this->statusMessage = 'me';
-        $user = $this->show($request, \Auth::user()->serial);
-        $user->additional(array_merge_recursive($user->additional, [
-            'additional' => [ 'token' => $request->bearerToken() ]
-        ]));
-        return $user;
-    }
+    use \iLaravel\Core\iApp\Http\Controllers\API\v1\Auth\Me;
 }
--- src/iapp/Http/Controllers/API/v1/Auth/MeUpdate.php
+++ src/iapp/Http/Controllers/API/v1/Auth/MeUpdate.php
@@ -10,37 +10,7 @@
 
 namespace iLaravel\iAuth\iApp\Http\Controllers\API\v1\Auth;
 
-use Illuminate\Support\Facades\Hash;
-
 trait MeUpdate
 {
-    public function me_update(\Illuminate\Http\Request $request)
-    {
-        $user = $this->model::find(auth()->id());
-        if ($request->password) {
-            $request->replace(['password' => Hash::make($request->password)]);
-        }
-        $update = [
-            'name' => (string)$request->name,
-            'family' => (string)$request->family,
-            'website' => (string)$request->website,
-            'gender' => (string)$request->gender
-        ];
-        if (isset($request->password))
-            $update['password'] = $request->password;
-        $avatar = $request->file('avatar');
-        $request->files->remove('avatar');
-        if ($avatar) {
-            $attachment = File::upload($request, 'avatar');
-            if ($attachment) {
-                $update['avatar_id'] = $attachment->id;
-                $this->statusMessage = $this->class_name() . " changed";
-            }
-            File::imageSize($attachment, 500);
-            File::imageSize($attachment, 250);
-            File::imageSize($attachment, 150);
-        }
-        $user->update($update);
-        return $user;
-    }
+    use \iLaravel\Core\iApp\Http\Controllers\API\v1\Auth\MeUpdate;
 }
